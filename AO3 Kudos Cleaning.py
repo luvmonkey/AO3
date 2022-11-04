4925ea91339ab25b70fc52199d7eebe9a79cbed3
Eva Cote <eva.rose.cote@gmail.com>
2022-11-04 12:47:05 -0700

    Creating unique lists

--- AO3 Kudos Cleaning.py
+++ AO3 Kudos Cleaning.py
@@ -19,10 +19,15 @@ top_100_kudos = top_100_kudos.astype({
     'user_id': 'string', 
     'title': 'string', 
     'username': 'string',
+    'fandoms': 'string',
     'rating': 'string',
+    'warnings': 'string',
+    'slash_categories': 'string',
     'freeforms': 'string',
     'status': 'string', 
     'date_updated': 'string', 
+    'relationships': 'string',
+    'characters': 'string',
     'summary': 'string', 
     'language': 'string', 
     'chapters': 'string'})
@@ -31,10 +36,14 @@ top_100_kudos = top_100_kudos.astype({
 # print(top_100_kudos.rating.unique())
 top_100_kudos['rating'] = pd.Categorical(top_100_kudos['rating'], ordered=True, categories=['General Audiences', 'Teen And Up Audiences', 'Mature', 'Explicit', 'Not Rated']) 
 
-
-# The freeform tags are a currently a list, recognized as a string. 
+# Some of our columns are a currently a list, recognized as a string. 
 # We need to clean this up so that we can use aggregate functions on the data.
+top_100_kudos['fandoms'] = top_100_kudos.fandoms.apply(eval)
 top_100_kudos['freeforms'] = top_100_kudos.freeforms.apply(eval)
+top_100_kudos['warnings'] = top_100_kudos.warnings.apply(eval)
+top_100_kudos['slash_categories'] = top_100_kudos.slash_categories.apply(eval)
+top_100_kudos['relationships'] = top_100_kudos.relationships.apply(eval)
+top_100_kudos['characters'] = top_100_kudos.characters.apply(eval)
 
 # The following shows that our lists are in fact now lists.
 for i, l in enumerate(top_100_kudos['freeforms']):
